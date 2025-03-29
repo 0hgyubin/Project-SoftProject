383e0fa282f810faed27c588f677a03fcd4db256
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2025-03-29 21:21:24 +0900

    투사체(검의 궤적) 위치가 플레이어 정 중앙에 생기는 현상 수정.

--- Assets/Scripts/SwordController.cs
+++ Assets/Scripts/SwordController.cs
@@ -17,7 +17,23 @@ public class SwordController: WeaponController
 
     protected override void FireProjectile(){
         if(currentProjectile == null){
-            currentProjectile = Instantiate(swordProjectilePrefab, transform.position, transform.rotation);//투사체 생성
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            
+            // 플레이어와 마우스 사이의 방향 계산
+            Vector3 direction = (mousePos - transform.position).normalized;
+            
+            // 원하는 오프셋 거리 (예: 1.0f)
+            float offsetDistance = 0.5f;
+            
+            // 스폰 위치: 플레이어 위치에서 direction 방향으로 offsetDistance만큼 전진
+            Vector3 spawnPosition = transform.position + direction * offsetDistance;
+            
+
+            Quaternion rotatedRotation = transform.rotation * Quaternion.Euler(0, 0, -90f);
+            // currentProjectile = Instantiate(swordProjectilePrefab, transform.position, transform.rotation);//투사체 생성
+            currentProjectile = Instantiate(swordProjectilePrefab, spawnPosition, rotatedRotation);//투사체 생성
             currentProjectile.transform.SetParent(transform);   //투사체의 부모를 검으로 설정
             currentProjectile.GetComponent<ProjectileController>().SetLifetime(0.4f);
         }
