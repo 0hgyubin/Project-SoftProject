5d369649f6753e326ec38db2c51f2367761e7134
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2025-04-06 21:38:28 +0900

    대규모 수정 후 정상작동 확인. 아직 자식클래스 안 만듬.

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 
-
+    [Header("Detection & Movement")]
     public float detectionRadius = 5f; // 플레이어를 탐지할 수 있는 반경
     public float attackRadius = 1f; // 플레이어를 공격할 수 있는 반경
     public float moveSpeed = 2f; // 적의 이동 속도
+
+    [Header("Health")]
     public float enemyHealth = 100f; // 적의 체력
 
-    public int playerDamage; // 플레이어가 적에게 가하는 데미지
+    [Header("Attack time")]
     public float attackDelay = 1f; // 공격 준비 시간
     public float attackDuration = 0.5f; // 공격 애니메이션 시간
+
+    [Header("Projectile Settings")]
     public GameObject ProjectilePrefab; // 투사체 프리팹
     public Transform shootTransform; // 투사체 발사 위치
     public float projectileSpeed = 5f; // 투사체 속도
     public int damage = 5; //투사체 맞았을 때 플레이어가 입는 데미지
     public float maxDistance = 5f; //투사체가 최대로 갈 수 있는 거리
-    public bool isMelee = true; //근거리인지 원거리인지
-    public bool isMoved = true;
 
-    private SpriteRenderer spriteRenderer;
+    [Header("bool")]
+    public bool isMelee = true; //근거리인지 원거리인지
+    public bool isMoved = true; 
+
+    public int playerDamage; // 플레이어가 적에게 가하는 데미지
+    protected SpriteRenderer spriteRenderer;
     public Animator weaponAnimator; //원거리 적 무기의 애니메이터(활 당기기 등의 모션 관련)
-    private Animator animator; //적 본인의 애니메이터
-    private Transform player; // 플레이어의 위치를 저장하는 변수
-    private bool isPlayerInRange = false; // 플레이어가 탐지 범위 내에 있는지 여부
-    private bool isPlayerInAttackRange = false; // 플레이어가 공격 범위 내에 있는지 여부
-    private bool isPreparingAttack = false; // 공격 준비 상태 여부
-    private float lastAttackTime; // 마지막 공격 시간
-    private Vector2 attackDirection; // 공격 방향을 저장하는 변수
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected Animator animator; //적 본인의 애니메이터
+    protected Transform player; // 플레이어의 위치를 저장하는 변수
+
+
+    protected bool isPlayerInRange = false; // 플레이어가 탐지 범위 내에 있는지 여부
+    protected bool isPlayerInAttackRange = false; // 플레이어가 공격 범위 내에 있는지 여부
+    protected bool isPreparingAttack = false; // 공격 준비 상태 여부
+    // private float lastAttackTime; // 마지막 공격 시간
+    protected Vector2 attackDirection; // 공격 방향을 저장하는 변수
+    
+    protected virtual void Start()
     {
         animator = GetComponent<Animator>();
         if (!isMelee)//원거리 공격 적이면 활, 총 등의 애니메이터를 받아옴.
@@ -43,17 +52,20 @@ public class EnemyController : MonoBehaviour
             }
         }
         //만약 활, 총 등에 애니메이션 없는 적인데 원거리 적이라면 말 해주세요 내용 더 추가해야 합니다
-        lastAttackTime = Time.time; // 현재 시간을 마지막 공격 시간으로 설정
+        // lastAttackTime = Time.time; // 현재 시간을 마지막 공격 시간으로 설정
         spriteRenderer = GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        if (player == null)
-        {
+        if (player == null){
             player = GameObject.FindGameObjectWithTag("Player").transform;        // 태그가 "Player"인 오브젝트의 위치를 가져옴
         }
+        if (player == null) return;
+
+
 
         float distanceToPlayer = Vector2.Distance(transform.position, player.position); // 적과 플레이어 사이의 거리 계산
 
@@ -61,17 +73,14 @@ public class EnemyController : MonoBehaviour
         FlipSpriteTowardsPlayer();
 
         //공격 준비 아니면서, 탐지 범위 내라면,  발사 각도 업데이트
-        if (!isPreparingAttack && distanceToPlayer <= detectionRadius) 
-        {
+        if (!isPreparingAttack && distanceToPlayer <= detectionRadius){
             UpdateShootRotation();
         }
 
         // 플레이어가 공격 범위 내에 있음
-        if (distanceToPlayer <= attackRadius)
-        {
+        if (distanceToPlayer <= attackRadius){
             isPlayerInAttackRange = true; 
-            if (!isPreparingAttack)
-            {
+            if (!isPreparingAttack){
                 StartCoroutine(PrepareAttack()); // 공격 준비 코루틴 시작
             }
         }
@@ -79,8 +88,7 @@ public class EnemyController : MonoBehaviour
         else if (distanceToPlayer <= detectionRadius)
         {
             isPlayerInRange = true;
-            if (isMoved)
-            {
+            if (isMoved){
                 FollowPlayer(); // 플레이어를 따라감
             }
         }
@@ -91,31 +99,36 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private void UpdateShootRotation()
-    {
-        Vector3 direction = player.position - shootTransform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        shootTransform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 180));
-    }
 
     //플레이어 위치에 따라 스프라이트 좌우 반전
-    private void FlipSpriteTowardsPlayer()
+    protected void FlipSpriteTowardsPlayer()
     {
-        if (player != null && spriteRenderer != null) 
+        if (spriteRenderer != null) 
         {
             spriteRenderer.flipX = player.position.x > transform.position.x;
         }
     }
 
-    void FollowPlayer(){
+    //발사 위치 회전 업데이트    
+    protected void UpdateShootRotation()
+    {
+        // if(shootTransform == null) return; 이 코드 없어도 문제 안생기면 삭제해도 됨.
+        Vector3 direction = player.position - shootTransform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        shootTransform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 180f));
+    }
+
+    protected void FollowPlayer(){
         if(isPlayerInRange && !isPlayerInAttackRange){
             Vector2 targetPosition = new Vector2(player.position.x, transform.position.y);
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
     }
 
-    IEnumerator PrepareAttack(){
+    protected IEnumerator PrepareAttack(){
         isPreparingAttack = true;
+        isMoved = false;
+
         if (!isMelee) //원거리 적의 무기 애니메이션 관련 코드
         {
             weaponAnimator.enabled = true;
@@ -124,22 +137,23 @@ public class EnemyController : MonoBehaviour
         else if (isMelee) //근거리 적의 공격 애니메이션 관련 코드
         {
             animator.SetBool("isAttack", true);
-            isMoved = false;
         }
+
         attackDirection = (player.position - transform.position).normalized;
         yield return new WaitForSeconds(attackDelay);
         AttackPlayer();
         yield return new WaitForSeconds(attackDuration);
+
         if (isMelee) //근거리 적의 공격 애니메이션 관련 코드
         {
             animator.SetBool("isAttack", false);
         }
         isPreparingAttack = false;
         isMoved = true;
-
+        
         // 공격 후 플레이어가 탐지 범위 내에 있으면 다시 쫓아감
-        if (Vector2.Distance(transform.position, player.position) > attackRadius && Vector2.Distance(transform.position, player.position) <= detectionRadius)
-        {
+         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        if (distanceToPlayer > attackRadius && distanceToPlayer <= detectionRadius){
             isPlayerInAttackRange = false;
             if(isMoved){
                 FollowPlayer();
@@ -147,11 +161,11 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    void AttackPlayer(){
+    protected void AttackPlayer(){
         Shoot(attackDirection);
     }
 
-    void Shoot(Vector2 direction){
+    protected void Shoot(Vector2 direction){
         GameObject projectile;
         if(!isMelee){
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
@@ -172,7 +186,7 @@ public class EnemyController : MonoBehaviour
 
     public void TakeDamage(float damage){
         enemyHealth -= damage;
-        if(enemyHealth <= 0){
+        if(enemyHealth <= 0f){
             Destroy(gameObject);
         }
     }
