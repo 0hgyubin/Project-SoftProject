516f446784d4384235cb4610296b50c551656e60
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2025-04-10 15:48:10 +0900

    WeaponRepository.cs 생성

---
+++ Assets/Scripts/Weapon/WeaponRepository.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class WeaponData {
+    public int weaponID;
+    public GameObject weaponPrefab; //무기 프리팹
+    public GameObject projectilePrefab; //투사체 프리팹
+    public float attackPower;
+    public float projectileLifetime; //무기마다 lifetime을 다르게 설정하기 위한 변수
+    public WeaponGrade grade;
+}
+
+[CreateAssetMenu(fileName = "WeaponRepository", menuName = "Game/Weapon Repository")]
+public class WeaponRepository : ScriptableObject {
+    public List<WeaponData> weapons;
+
+    public GameObject GetWeaponPrefabByID(int id) {
+        foreach (var weapon in weapons) {
+            if (weapon.weaponID == id) {
+                return weapon.weaponPrefab;
+            }
+        }
+        Debug.LogError("Weapon with ID " + id + " not found!");
+        return null;
+    }
+    
+    public WeaponData GetWeaponDataByID(int id) {
+        foreach (var weapon in weapons) {
+            if (weapon.weaponID == id) {
+                return weapon;
+            }
+        }
+        Debug.LogError("Weapon data with ID " + id + " not found!");
+        return null;
+    }
+}
